e7d55db242566b624ac90a37a56601fb884e5180
theotsori <theodoretsori@gmail.com>
2023-03-01 09:42:39 +0300

    tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,11 +34,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left && tree->right)
-	{
-		left_height = (int)binary_tree_height(tree->left);
-		right_height = (int)binary_tree_height(tree->right);
-	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
