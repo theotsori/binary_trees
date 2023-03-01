fed3858e4462a09b89815622ea41866d097505cb
theotsori <theodoretsori@gmail.com>
2023-03-01 09:48:34 +0300

    tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,12 +10,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (!tree)
+	if (tree == NULL)
+	{
 		return (0);
+	}
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
+	}
 }
-
