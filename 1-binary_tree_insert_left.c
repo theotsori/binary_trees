ae6c5efe522818d196e807f76bb54054177547bd
theotsori <theodoretsori@gmail.com>
2023-03-01 09:04:07 +0300

    tree left update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,20 +9,34 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
-	binary_tree_t *new_node = binary_tree_node(parent, value);
-
-	if (parent->left != NULL)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
 	{
-		parent->left->parent = new_node;
-		new_node->left = parent->left;
+		return (NULL);
 	}
 
-	parent->left = new_node;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = NULL;
 
-	return (new_node);
+	if (parent->left == NULL)
+	{
+		parent->left = new_node;
+		new_node->left = NULL;
+	}
+	else
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+		parent->left = new_node;
+	}
+
+	return new_node;
 }
