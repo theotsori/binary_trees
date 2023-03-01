5fcd59b5a605cf6a0ff00c2cb4b4acda97a61abd
theotsori <theodoretsori@gmail.com>
2023-03-01 08:49:35 +0300

    tree left update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,11 +16,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	binary_tree_t *new_node = binary_tree_node(parent, value);
 
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-
 	if (parent->left != NULL)
 	{
 		parent->left->parent = new_node;
