ab6267eb3d438bc7880da16988afde164a66d368
theotsori <theodoretsori@gmail.com>
2023-03-02 11:38:18 +0300

    tree is avl

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,67 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_avl - Checks if a binary tree is a valid AVL Tree
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid AVL Tree, 0 otherwise
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int left_height, right_height, height_diff;
+
+	if (tree == NULL)
+		return (1);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	height_diff = left_height - right_height;
+
+	if (abs((int)height_diff) > 1)
+		return (0);
+
+	return (binary_tree_is_avl(tree->left) && binary_tree_is_avl(tree->right));
+}
+
+/**
+ * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search Tree
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid BST, 0 otherwise
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (tree->left != NULL && tree->left->n >= tree->n)
+		return (0);
+
+	if (tree->right != NULL && tree->right->n <= tree->n)
+		return (0);
+
+	if (!binary_tree_is_bst(tree->left) || !binary_tree_is_bst(tree->right))
+		return (0);
+
+	return (1);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: The height of the tree starting
+ * from the root node, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
