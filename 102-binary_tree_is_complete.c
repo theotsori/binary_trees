977c7a81a808ef8934d7860b9a30018154abbc7d
theotsori <theodoretsori@gmail.com>
2023-03-02 08:42:33 +0300

    Is Binary Search Tree

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,51 +1,62 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is_complete - Checks if a binary tree is complete
- * @tree: Pointer to the root node of the tree to check
- * Return: 1 if the tree is complete, 0 otherwise
+ * binary_tree_is_complete - checks if a binary tree is complete
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if the binary tree is complete, 0 otherwise
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	queue_t *queue = NULL;
-	const binary_tree_t *node = NULL;
-	int is_complete = 1, has_missing = 0, *queue = NULL;
+	size_t nodes_count, i;
+	binary_tree_t **queue, *node;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-	queue = queue_create();
-	if (!queue)
+
+	nodes_count = binary_tree_size(tree);
+
+	queue = malloc(sizeof(*queue) * nodes_count);
+	if (queue == NULL)
 		return (0);
-	queue_push(queue, (void *)tree);
-	while (!queue_is_empty(queue))
+
+	queue[0] = (binary_tree_t *)tree;
+	i = 0;
+
+	while (i <= nodes_count - 1)
 	{
-		node = (const binary_tree_t *)queue_pop(queue);
-		if (node->left)
+		node = queue[i];
+
+		if (node->left != NULL)
 		{
-			if (has_missing)
-			{
-				is_complete = 0;
-				break;
-			}
-			queue_push(queue, (void *)node->left);
+			queue[(2 * i) + 1] = node->left;
+			if (node->right != NULL)
+				queue[(2 * i) + 2] = node->right;
+			else if (node->left->left != NULL || node->left->right != NULL)
+				return (0);
 		}
-		else
-		{
-			has_missing = 1;
-		}
-		if (node->right)
-		{
-			if (has_missing)
-			{
-				is_complete = 0;
-				break;
-			}
-			queue_push(queue, (void *)node->right);
-		}
-		else
-		{
-			has_missing = 1;
-		}
+		else if (node->right != NULL)
+			return (0);
+
+		++i;
 	}
-	queue_delete(queue);
-	return (is_complete);
+
+	free(queue);
+	return (1);
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: A pointer to the node to measure size
+ *
+ * Return: Size ot the measured tree
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is a valid BST, and 0 otherwise
+*/
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	static const binary_tree_t * prev = NULL;
+
+	if (tree == NULL)
+		return (0);
+
+	if (!binary_tree_is_bst(tree->left))
+		return (1);
+
+	if (prev != NULL && prev->n >= tree->n)
+		return (1);
+	prev = tree;
+
+	if (!binary_tree_is_bst(tree->right))
+		return (0);
+
+	return (0);
+}
